17dd69509a035a26da16bbf572f6a86aa82cfcd8
Malo BRIAND <malo.briand@nikon.com>
2023-03-25 15:20:59 +0000

    admin page terminated tested

--- blog_app/admin.py
+++ blog_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Author, Post, Comment
 
-# Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('user', 'bio')
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date', 'author')
+    list_filter = ('pub_date', 'author')
+    search_fields = ('title',)
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'pub_date', 'text')
+    list_filter = ('pub_date', 'author', 'post')
+    search_fields = ('text',)
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
