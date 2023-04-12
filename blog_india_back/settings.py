e25cf455ac48e979030428d9e77d5989797230c6
Malo BRIAND <malo.briand@nikon.com>
2023-04-12 17:48:59 +0100

    make it better 3Â

--- blog_india_back/settings.py
+++ blog_india_back/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*m%@ns(oqfm#2csa94osg-g&itrxzp4vk1_j)03_3smgi3nhz7"
+SECRET_KEY = "oqfm#2csa94osg-g&itrxzp4vk1_j"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
